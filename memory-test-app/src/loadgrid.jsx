7ad3ac4c28aa57439f1653756c32d790ceb7f2f0
robsomers900 <64647442+robsomers900@users.noreply.github.com>
2025-02-06 17:08:35 +0000

    Create LoadGrid file and functionality and LoadGameOver file

--- memory-test-app/src/loadgrid.jsx
+++ memory-test-app/src/loadgrid.jsx
@@ -1,35 +1,45 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 function getRandomInt(range){
     return Math.floor(Math.random() * range)
 }
 
 export default function LoadGrid() {
+    const [clickedValues, setClickedValues] = useState([])
+    const [lastClicked, setLastClicked] = useState(null)
     const [value, setValue] = useState([getRandomInt(100),getRandomInt(100),getRandomInt(100),getRandomInt(100)])
-    const handleButtonClick = () => {
+    const handleButtonClick = (valueClicked) => {
+        setClickedValues(prevClickedValues => [...prevClickedValues, valueClicked])
+        setLastClicked(valueClicked)
         setValue([getRandomInt(100),getRandomInt(100),getRandomInt(100),getRandomInt(100)])
     }
-
+    useEffect(() => {
+        console.log(clickedValues)
+        console.log(lastClicked)
+        //if last clicked in clickedvalues, modal displays saying you lost - use length of array for score
+        let clickedValuesExcludingLast = clickedValues.slice(0, -1)
+        console.log(clickedValuesExcludingLast.includes(lastClicked))
+    }, [handleButtonClick])
     return(
         <>
             <div className='number-grid'>
                 <div className='number-button-container'>
-                    <button className='number-button' onClick={handleButtonClick}>
+                    <button className='number-button' onClick={() => handleButtonClick(value[0])}>
                         {value[0]}
                     </button>
                 </div>
                 <div className='number-button-container'>
-                    <button className='number-button' onClick={handleButtonClick}>
+                    <button className='number-button' onClick={() => handleButtonClick(value[1])}>
                         {value[1]}
                     </button>
                 </div>
                 <div className='number-button-container'>
-                    <button className='number-button' onClick={handleButtonClick}>
+                    <button className='number-button' onClick={() => handleButtonClick(value[2])}>
                         {value[2]}
                     </button>
                 </div>
                 <div className='number-button-container'>
-                    <button className='number-button' onClick={handleButtonClick}>
+                    <button className='number-button' onClick={() => handleButtonClick(value[3])}>
                         {value[3]}
                     </button>
                 </div>
